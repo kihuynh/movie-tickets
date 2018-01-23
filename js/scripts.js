8ad988894e59809930e04e8f7717be186e3e7cd8
Kimberly Huynh and Maggie Summers <maggie@>
2018-01-23 14:28:01 -0800

    add discounts via js branching

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,11 @@ function Ticket(movie, time, age) {
 
   if (seniorDiscount(age) === true) {
     price = price-1.5;
+  } if (timeDiscount(time) === true) {
+    price = price-2;
+  } if (oldMovieDiscount(movie) === false) {
+    price = price-5;
   }
-
 
   this.price = price;
 };
@@ -21,6 +24,21 @@ function seniorDiscount(age) {
   }
 }
 
+function timeDiscount(time) {
+  if (time === "Matinee") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function oldMovieDiscount(movie) {
+  if (movie !== "Star Wars: Force Awakens") {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 Ticket.prototype.movieReceipt = function() {
   return this.movie + ", " + this.time + ", " + this.age + ", $" + this.price;
