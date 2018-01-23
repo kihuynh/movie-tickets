fc4f38597b0c24a75da3e3637b584d158d0c8b61
Kimberly Huynh and Maggie Summers <maggie@>
2018-01-23 13:39:50 -0800

    add js function to create pparameter

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,27 @@ function Ticket(movie, time, age) {
   this.movie = movie;
   this.time = time;
   this.age = age;
-  //this.price = price;
-}
 
-function Price(matinee, evening) {
-  this.matinee = matinee;
-  this.evening = evening;
-}
+  var price = 10;
 
-Ticket.prototype.movieReceipt = function() {
-  return this.movie + ", " + this.time;
-}
+  if (seniorDiscount(age) === true) {
+    price = price-1.5;
+  }
 
-Price.prototype.moviePrice = function () {
-  return
+
+  this.price = price;
+};
+
+function seniorDiscount(age) {
+  if (age === "Senior") {
+    return true;
+  } else {
+    return false;
+  }
 }
+// Ticket.prototype.movieReceipt = function() {
+//   return this.movie + ", " + this.time + ", " + this.age;
+// }
 
 
 //user
@@ -29,25 +35,20 @@ $(document).ready(function() {
     var selectAge = $("select#age").val();
 
     var newTicket = new Ticket(selectMovie, selectTime, selectAge);
-
-    var priceMatinee = "5";
-    var priceKid = "6";
-    var priceSenior = "7";
-    var priceEvening = "10";
-
-    if (selectAge === "Senior") {
-      $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + ", " + " $" + priceSenior + ".");
-    } else if (selectAge === "Adult" && selectTime === "Evening") {
-      $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + ", " + " $" + priceEvening + ".");
-    } else if (selectAge === "Adult" && selectTime === "Matinee") {
-      $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + ", " + " $" + priceMatinee + ".");
-    } else if (selectAge === "Kid") {
-      $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + ", " + " $" + priceKid + ".");
-    }
-    //$("#movie-info").show().append("<p>" + newTicket.movieReceipt() + " for" + " $" + priceMatinee + ".");
-
-
-    //"</p>" + "<button type='click' class='btn btn-info' id='price'>" + "Show Me My Price" + "</button>");
-
+    console.log(newTicket);
+    // var priceMatinee = "5";
+    // var priceKid = "6";
+    // var priceSenior = "7";
+    // var priceEvening = "10";
+    //
+    // if (selectAge === "Senior") {
+    //   $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + "</br>" + "Price = " + " $" + priceSenior);
+    // } else if (selectAge === "Adult" && selectTime === "Evening") {
+    //   $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + "</br>" + "Price = " + " $" + priceEvening);
+    // } else if (selectAge === "Adult" && selectTime === "Matinee") {
+    //   $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + "</br>" + "Price = " + " $" + priceMatinee);
+    // } else if (selectAge === "Kid") {
+    //   $("#movie-info").show().append("<p>" + newTicket.movieReceipt() + "</br>" + "Price = " + " $" + priceKid);
+    // }
   });
 });
